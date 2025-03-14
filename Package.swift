26d83cb95d403e83bc61e4dbec63513bb7ae5262
Niklas Bergius <niklas@bergius.org>
2025-03-14 18:28:29 +0100

    Capturing full screen every second for finding pixels.

--- Package.swift
+++ Package.swift
@@ -10,12 +10,16 @@ let package = Package(
         .library(
             name: "screenstream",
             type: .dynamic,
-            targets: ["screenstream"])
+            targets: ["screenstream"]
+        ),
+        .library(
+            name: "ScreenStream",
+            targets: ["screenstream"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "screenstream")
+            name: "screenstream"
+        )
     ]
 )
--- Sources/screenstream/ScreenCapturer.swift
+++ Sources/screenstream/ScreenCapturer.swift
@@ -28,12 +28,14 @@ struct ScreenCapturerConfig {
     let width: Int
     let height: Int
     let frameRate: Int32
+    let fullScreenFrameRate: Int32
 
     init(
         displayId: Int32,
         x: Int32, y: Int32,
         width: Int32, height: Int32,
-        frameRate: Int32 = 30
+        frameRate: Int32 = 30,
+        fullScreenFrameRate: Int32 = 1
     ) {
         self.displayId = Int(displayId)
         self.x = Int(x)
@@ -41,24 +43,27 @@ struct ScreenCapturerConfig {
         self.width = Int(width)
         self.height = Int(height)
         self.frameRate = Int32(frameRate)
+        self.fullScreenFrameRate = Int32(fullScreenFrameRate)
     }
 }
 
 @available(macOS 12.3, *)
-class ScreenCapturer: NSObject, SCStreamOutput {
+class ScreenCapturer: NSObject {
     private let config: ScreenCapturerConfig
-    private var frameBuffer: Data
     private let onFrameCaptured: (Data) -> Void
+    private let onFullScreenCaptured: (Data) -> Void
 
-    private var stream: SCStream?
+    private var regionOutput: CaptureOutput?
+    private var fullScreenOutput: CaptureOutput?
 
     init(
         config: ScreenCapturerConfig,
-        onFrameCaptured: @escaping (Data) -> Void
+        onFrameCaptured: @escaping (Data) -> Void,
+        onFullScreenCaptured: @escaping (Data) -> Void
     ) {
         self.config = config
-        self.frameBuffer = Data(count: Int(config.width * config.height * 4))
         self.onFrameCaptured = onFrameCaptured
+        self.onFullScreenCaptured = onFullScreenCaptured
     }
 
     func startCapturing() async throws {
@@ -68,35 +73,33 @@ class ScreenCapturer: NSObject, SCStreamOutput {
                     false,
                     onScreenWindowsOnly: true)
 
-            guard let display = shareableContent.displays.first(where: {
-                $0.displayID == config.displayId
-            }) else {
+            guard
+                let display = shareableContent.displays.first(where: {
+                    $0.displayID == config.displayId
+                })
+            else {
                 throw CaptureError.noDisplaysFound
             }
 
-            let filter = SCContentFilter(
-                display: display, excludingApplications: [], exceptingWindows: [])
+            let regionOutput = CaptureOutput(
+                display: display,
+                x: config.x, y: config.y,
+                width: config.width, height: config.height,
+                frameRate: config.frameRate,
+                onFrameCaptured: onFrameCaptured)
 
-            let streamConfig = SCStreamConfiguration()
-            streamConfig.width = Int(config.width)
-            streamConfig.height = Int(config.height)
-            streamConfig.pixelFormat = kCVPixelFormatType_32BGRA
-            streamConfig.minimumFrameInterval = CMTime(
-                value: 1, timescale: config.frameRate)
-            streamConfig.sourceRect = CGRect(
-                x: Int(config.x), y: Int(config.y),
-                width: Int(config.width), height: Int(config.height))
+            let fullScreenOutput = CaptureOutput(
+                display: display,
+                x: 0, y: 0,
+                width: display.width, height: display.height,
+                frameRate: 1,
+                onFrameCaptured: onFullScreenCaptured)
 
-            let stream = SCStream(filter: filter, configuration: streamConfig, delegate: nil)
-            self.stream = stream
+            try await regionOutput.start()
+            try await fullScreenOutput.start()
 
-            try stream.addStreamOutput(
-                self, type: .screen, sampleHandlerQueue: DispatchQueue.global(qos: .userInteractive)
-            )
-
-            try await stream.startCapture()
-
-            // catch everything but CaptureError.noDisplaysFound
+            self.regionOutput = regionOutput
+            self.fullScreenOutput = fullScreenOutput
         } catch CaptureError.noDisplaysFound {
             throw CaptureError.noDisplaysFound
         } catch {
@@ -105,6 +108,75 @@ class ScreenCapturer: NSObject, SCStreamOutput {
     }
 
     func stopCapturing() async throws {
+        if let regionOutput = regionOutput {
+            try await regionOutput.stop()
+            self.regionOutput = nil
+        }
+        if let fullScreenOutput = fullScreenOutput {
+            try await fullScreenOutput.stop()
+            self.fullScreenOutput = nil
+        }
+    }
+}
+
+@available(macOS 12.3, *)
+class CaptureOutput: NSObject, SCStreamOutput {
+    private let display: SCDisplay
+    private let x: Int
+    private let y: Int
+    private let width: Int
+    private let height: Int
+    private let frameRate: Int32
+    private let onFrameCaptured: (Data) -> Void
+
+    private var frameBuffer: Data
+    private var stream: SCStream?
+
+    init(
+        display: SCDisplay,
+        x: Int, y: Int,
+        width: Int, height: Int,
+        frameRate: Int32,
+        onFrameCaptured: @escaping (Data) -> Void
+    ) {
+        self.display = display
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.frameRate = frameRate
+        self.onFrameCaptured = onFrameCaptured
+
+        self.frameBuffer = Data(count: Int(width * height * 4))
+    }
+
+    func start() async throws {
+        let filter = SCContentFilter(
+            display: display, excludingApplications: [], exceptingWindows: [])
+
+        let streamConfig = SCStreamConfiguration()
+        streamConfig.width = width
+        streamConfig.height = height
+        streamConfig.pixelFormat = kCVPixelFormatType_32BGRA
+        streamConfig.minimumFrameInterval = CMTime(
+            value: 1, timescale: frameRate)
+        streamConfig.sourceRect = CGRect(
+            x: x, y: y,
+            width: width, height: height)
+
+        let stream = SCStream(filter: filter, configuration: streamConfig, delegate: nil)
+        self.stream = stream
+
+        try stream.addStreamOutput(
+            self,
+            type: .screen,
+            sampleHandlerQueue: DispatchQueue.global(qos: .userInteractive)
+        )
+
+        try await stream.startCapture()
+    }
+
+    func stop() async throws {
         guard let stream = stream else {
             return
         }
@@ -129,11 +201,11 @@ class ScreenCapturer: NSObject, SCStreamOutput {
         }
 
         let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
-        let expectedBytesPerRow = config.width * 4
+        let expectedBytesPerRow = self.width * 4
 
         frameBuffer.removeAll(keepingCapacity: true)
 
-        for row in 0..<config.height {
+        for row in 0..<self.height {
             let rowStart = baseAddress.advanced(by: row * bytesPerRow)
             frameBuffer.append(Data(bytes: rowStart, count: expectedBytesPerRow))
         }
